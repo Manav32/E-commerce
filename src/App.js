d868c65c85c13ce5b98ebf5565c30741e21baebb
dunnjacoba <101910318+dunnjacoba@users.noreply.github.com>
2022-08-18 17:58:34 -0500

    'Fixes routing, removes unneeded routing imports'

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
 import React from "react";
 import MasterLayout from "./assests/Layout/admin/MasterLayout";
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
-import Main from "./routes/Main";
 
 const App = () => {
   return (
     <div className="App">
-      {/* <Routes> */}
-          <MasterLayout />
-        {/* </Routes> */}
-          
-      
+      <MasterLayout />
     </div>
   );
-}
+};
 
 export default App;
